fde5ee6bc642383e2a7e1e7f38dab3cdac07c3ec
Veronique Cadet <cadetvero@gmail.com>
2023-02-24 11:49:01 -0500

    hi

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ has_many :skills, through: :user_skills
 
 has_many :proposed_barters, class_name: :Barter, foreign_key: "proposer_id"
 has_many :recieved_barters, class_name: :Barter, foreign_key: "recipient_id"
+
+has_secure_password
 end
--- db/migrate/20230223181019_create_users.rb
+++ db/migrate/20230223181019_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :first_name
       t.string :last_name
       t.string :email
-      t.string :password
+      t.string :password_digest
       t.integer :age
       t.string :bio
       t.string :picture
